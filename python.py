dcd95fb725ac73f03d8162c723188049e7234667
unknown <watcherback22@gmail.com>
2025-08-21 22:28:08 +0530

    sf

--- python.py
+++ python.py
@@ -1,22 +1,5 @@
 rtgudfjklhgtrjfecfd bfkv oijgkfdoivjlkdmfgfvobiklgfdjmcidgbgjflkdmcn vjhdfojcflkxnjehrfjkd
 
-# --- SETUP SELENIUM ---
-options = Options()
-options.add_argument("--disable-popup-blocking")
-driver = webdriver.Chrome(options=options)
-
-# --- STEP 1: Open the site ---
-driver.get("https://studentportal.inokomis.in/Newstudent/WeLectures")  # 🔁 Replace with actual URL
-driver.maximize_window()
-time.sleep(95)
-#hello world
-# --- STEP 2: Find all "View" buttons ---
-view_buttons = driver.find_elements(By.XPATH, "//button[text()='View']")
-
-print(f"Found {len(view_buttons)} videos.")
-
-# --- STEP 3: Loop through each button ---
-for index in range(len(view_buttons)):
     # Re-locate buttons every time to avoid stale element reference
     view_buttons = driver.find_elements(By.XPATH, "//button[text()='View']")
     
